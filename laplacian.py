281a4e9603cf01d9465b98f69db651f8691fbcb5
fredmloth <frederiekeloth@gmail.com>
2025-03-13 12:11:13 +0100

    vectorized laplacian

--- laplacian.py
+++ laplacian.py
@@ -1,32 +1,38 @@
 import numpy as np
 import scipy.linalg
 
+import matplotlib.pyplot as plt
+
 def laplacian_matrix(length):
+    """Creates the Matrix M of the eigenvalue problem. Length is the 
+    (length x length) size of a grid."""
 
     N = length*length
 
     # Create zero matrix, size of (N*N) x (N*N) 
     matrix_M = np.zeros((N, N))
 
-    # Create diagonals from 5-point-stencil
-    for i in range(N):
-        for j in range(N):
-        
-            if i == j:
-                matrix_M[i,j] = -4
+    # i == j
+    np.fill_diagonal(matrix_M, -4)
 
-            if i == (j-1):
-                matrix_M[i,j] = 1
+    # i == j-1
+    np.fill_diagonal(matrix_M[1:], 1)
 
-            if i == (j+1):
-                matrix_M[i,j] = 1
+    # i == j+1
+    np.fill_diagonal(matrix_M[:, 1:], 1)
 
-            if i == (j-4):
-                matrix_M[i,j] = 1
+    # i == j-4
+    np.fill_diagonal(matrix_M[4:], 1)
 
-            if i == (j+4):
-                matrix_M[i,j] = 1
+    # i == j+4
+    np.fill_diagonal(matrix_M[:, 4:], 1)
 
     return matrix_M
 
-print(laplacian_matrix(4))
+N = 4
+modes = 6
+
+lap_M = laplacian_matrix(N)
+print(lap_M)
+
+eigenvalues, eigenvectors = scipy.linalg.eigh(lap_M, modes)
