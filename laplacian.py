00e21082941e7e38bd9356564930e917e9b51fda
fredmloth <frederiekeloth@gmail.com>
2025-03-13 11:25:49 +0100

    sets 2 diagonals out of 5

--- laplacian.py
+++ laplacian.py
@@ -1,10 +1,12 @@
 import numpy as np
 import scipy.linalg
 
-def laplacian_matrix(N):
+def laplacian_matrix(length):
+
+    N = length*length
 
     # Create zero matrix, size of (N*N) x (N*N) 
-    matrix_M = np.zeros((N*N, N*N))
+    matrix_M = np.zeros((N, N))
 
     # Create diagonals from 5-point-stencil
     for i in range(N):
@@ -12,4 +14,10 @@ def laplacian_matrix(N):
         
             if i == j:
                 matrix_M[i,j] = -4
+
+            if i == (j-1):
+                matrix_M[i,j] = 1
+    print(matrix_M)
     return
+
+laplacian_matrix(4)
