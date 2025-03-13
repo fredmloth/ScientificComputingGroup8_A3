0dd5c73d99c4ae3756df328bd5ed3b45472d1710
fredmloth <frederiekeloth@gmail.com>
2025-03-13 13:32:53 +0100

    trying to fix visualisation

--- diagonal_matrix.py
+++ diagonal_matrix.py
@@ -41,9 +41,27 @@ eigenvalues, eigenvectors = scipy.linalg.eigh(diag_M)
 
 # Only take smallest number and each eigenvector column is a mode so 
 # need similar nr of columns 
-smallest_nr = 6
 eigenvalues = eigenvalues[:modes] 
 eigenvectors = eigenvectors[:, :modes]
+eigenmodes = eigenvectors.reshape(N, N, -1)
 
-print(eigenvalues)
+#print(eigenvectors)
+#print(eigenmodes)
 
+def visualize_multiple_modes(eigenmodes, N, num_modes=6):
+    """Plots eigenmodes"""
+    fig, axes = plt.subplots(2, 3, figsize=(12, 8))
+
+    # Flatten the axes for correct image rendering
+    axes = axes.flatten()
+    
+    for i in range(num_modes):
+        axes[i].imshow(eigenmodes[:, :, i], cmap='coolwarm')
+        axes[i].set_title(f"Mode = {i+1}")
+        #axes[i].axis('off')
+
+    #plt.colorbar(label='Amplitude')
+    plt.tight_layout()
+    plt.show()
+
+#visualize_multiple_modes(eigenmodes, N, num_modes=6)
