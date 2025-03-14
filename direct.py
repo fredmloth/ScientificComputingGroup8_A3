6a620b0ff252e82edf32bbe9bb2b305038be5d35
kushhhhs <kushnavas@gmail.com>
2025-03-14 19:52:10 +0100

    Create a function to map from 2D indices to 1d index

--- direct.py
+++ direct.py
@@ -13,12 +13,28 @@ def create_disk_mask(X, Y, radius):
     
     return X**2 + Y**2 <= radius**2
 
+
+def create_index_mapping(mask):
+    """Create a mapping from 2D grid indices to 1D system indices."""
+    
+    N = mask.shape[0]
+    map_2d_to_1d = np.zeros((N, N), dtype=int) - 1  # We initialize the matrix with -1 and not zero since the indexing starts from zero 
+    counter = 0
+    for i in range(N):
+        for j in range(N):
+            if mask[i, j]:
+                map_2d_to_1d[i, j] = counter
+                counter += 1
+
+    return map_2d_to_1d, counter
+
 N = 100  
 radius = 2.0 
 source_x = 0.6  
 source_y = 1.2 
 X, Y, h = create_grid(N, radius)
 mask = create_disk_mask(X, Y, radius)
+map_2d_to_1d, n_interior = create_index_mapping(mask)
 
 
 
